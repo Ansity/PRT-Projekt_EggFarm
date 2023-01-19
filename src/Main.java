37405d9d68f5e9a695297f1de388a850be2a9c57
Ansity <bestps4@gmail.com>
2023-01-19 17:14:42 +0100

    Änderungen der UI wurden durchgeführt: Küken als Bild eingefügt. UI wurde optisch verbessert. Es werden mehr Informationen angezeigt, um Spielern die Spielfreude zu erhöhen.

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,41 @@
 import java.util.Scanner;
 
 public class Main {
-    static String userName = "";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Henhouse henhouse = new Henhouse();
-        int dayCounter = 1;
-
-        LeaderboardController leaderboardController = new LeaderboardController();
 
         boolean endProgram = false;
-
-        initializeGame();
-
         while (!endProgram) {
             try {
                 henhouse.chickenStatus();
 
                 System.out.println();
                 System.out.println();
-                System.out.println("Hauptmenü");
-                System.out.println("---------");
+                System.out.println("             Hauptmenü");
+                System.out.println("              .=\"\"=.\n" +
+                                              "             / _  _ \\\n" +
+                                              "            |  d  b  |\n" +
+                                              "            \\   /\\   /\n" +
+                                              "           ,/'-=\\/=-'\\,\n" +
+                                              "          / /        \\ \\\n" +
+                                              "         | /          \\ |\n" +
+                                              "         \\/ \\        / \\/\n" +
+                                              "             '.    .'\n" +
+                                              "             _|`~~`|_\n" +
+                                              "             /|\\  /|\\                              ");
+                System.out.println("-----------------------------------------------------");
                 System.out.println();
-                System.out.println("A   -> Alle Eier verkaufen");
-                System.out.println("C   -> Chinesisches Huhn kaufen");
-                System.out.println("O   -> Österreichisches Huhn kaufen");
-                System.out.println("D   -> Deutsches Huhn kaufen");
-                System.out.println("U   -> Amerikanisches Huhn kaufen");
-                System.out.println("AR -> Huhn zu Argeta-Aufstrich verarbeiten");
-                System.out.println("DO -> Huhn zum Tierarzt schicken");
-                System.out.println("N   -> Nichts tun");
-                System.out.println("E   -> Spiel beenden (aufgeben)");
+                System.out.println("()    A   -> Alle Eier verkaufen                              ()");
+                System.out.println("()    C   -> Chinesisches Huhn kaufen (€100 + 5 Eier)         ()");
+                System.out.println("()    O   -> Österreichisches Huhn kaufen (€150 + 4 Eier)     ()");
+                System.out.println("()    D   -> Deutsches Huhn kaufen (€50 + 7 Eier)             ()");
+                System.out.println("()    U   -> Amerikanisches Huhn kaufen (€80 + 8 Eier)        ()");
+                System.out.println("()    AR  -> Huhn zu Argeta-Aufstrich verarbeiten (Gw: €50)   ()");
+                System.out.println("()    DO  -> Huhn zum Tierarzt schicken (€50 + 4 Eier)        ()");
+                System.out.println("()    N   -> Nichts tun                                       ()");
+                System.out.println("()    E   -> Spiel beenden (aufgeben)                         ()");
                 System.out.print("\nChoice:");
                 String choice = scanner.nextLine().toUpperCase();
                 switch (choice) {
@@ -91,39 +95,20 @@ public class Main {
                         henhouse.sendChickenToDoctor(chickenNumberInput);
                     }
                     case "N" -> endProgram = false;
-                    case "E" -> {
-                        endProgram = true;
-                        leaderboardController.addNewScoreEntry(userName, henhouse.getBank().getMoney(), dayCounter);
-                        leaderboardController.showLeaderboard();
-                    }
-                    default -> System.out.println("Ungültige Auswahl!");
+                    case "E" -> endProgram = true;
+                    default -> System.out.println("UngültigeAuswahl!");
                 }
             } catch (BankruptcyException e) {
                 System.out.println("SIE HABEN VERLOREN!!!");
                 System.out.println("In der Kassa ist kein Geld mehr!");
-                leaderboardController.addNewScoreEntry(userName, henhouse.getBank().getMoney(), dayCounter);
                 endProgram = true;
-                leaderboardController.showLeaderboard();
             } catch (MissingEggsException e) {
                 System.out.println("SIE HABEN VERLOREN!!!");
                 System.out.println("Es sind keine Eier da, um ein Huhn zu erzeugen und kein Huhn im Stall, welches Eier legt!");
-                leaderboardController.addNewScoreEntry(userName, henhouse.getBank().getMoney(), dayCounter);
                 endProgram = true;
-                leaderboardController.showLeaderboard();
             }
 
             henhouse.dailyRun();
-            dayCounter++;
         }
-
-
-    }
-
-    public static void initializeGame() {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter your username");
-
-        userName = myObj.nextLine();
-        System.out.println("Your game will begin now " + userName + "\n");
     }
 }
